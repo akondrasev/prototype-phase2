11580eeaa45d78438d6ab0c12efa8d9aaf1f639a
Anton Kondrasev <anton.kondrasev@live.com>
2015-04-29 17:19:52 +0300

    second commit

--- src/main/java/inc/action/PageLandingAction.java
+++ src/main/java/inc/action/PageLandingAction.java
@@ -3,5 +3,8 @@ package inc.action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class PageLandingAction extends ActionSupport{
+    public String execute(){
+        return SUCCESS;
+    }
 
 }
